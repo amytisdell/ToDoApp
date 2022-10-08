6a5e49fbe8d03a94cddcadd2b23810d958961f90
Amy Tisdell <amytisdell@icloud.com>
2022-10-08 00:48:22 -0400

    all files committed

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 let id = 0
 
 document.getElementById('add').addEventListener('click', () => {
-    let createDate = new Date();
     let table = document.getElementById('list');
     let row = table.insertRow(1);
     row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('new-task').value;
-    row.insertCell(1).innerHTML = `${createDate.getFullYear()} - ${createDate.getMonth()+1}-${createDate.getDate()}`;
-    row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
-    row.insertCell(3).innerHTML = document.getElementById('new-end-date').value;
-    let actions = row.insertCell(4);
+    row.insertCell(0).innerHTML = document.getElementById("employee-name").value;
+    row.insertCell(1).innerHTML = document.getElementById('birthday').value;
+    row.insertCell(2).innerHTML = document.getElementById("Favorite-cake-flavor").value;
+    // row.insertCell(3).innerHTML = document.getElementById("allergies").value;
+
+    let actions = row.insertCell(3);
     actions.appendChild(createDeleteButton(id++));
-    document.getElementById('new-task').value = ``;
+    document.getElementById('employee-name').value = ``;
+    document.getElementById('birthday').value = ``;
+    document.getElementById("Favorite-cake-flavor").value = ``;
+    // document.getElementById('allergies').radioChecked = false
 });
 
 function createDeleteButton(id) {
